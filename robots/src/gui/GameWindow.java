1edbe932ab46c60333ed80cdcf5df646044c2066
Ryan Gosling <vjlev671games@gmail.com>
2025-09-28 20:21:28 +0500

    Oбновил GameWindow под новую логику

--- robots/src/gui/GameWindow.java
+++ robots/src/gui/GameWindow.java
@@ -1,20 +1,17 @@
 package gui;
 
-import java.awt.BorderLayout;
-
 import javax.swing.JInternalFrame;
-import javax.swing.JPanel;
 
-public class GameWindow extends JInternalFrame
-{
-    private final GameVisualizer m_visualizer;
-    public GameWindow() 
-    {
-        super("Игровое поле", true, true, true, true);
-        m_visualizer = new GameVisualizer();
-        JPanel panel = new JPanel(new BorderLayout());
-        panel.add(m_visualizer, BorderLayout.CENTER);
-        getContentPane().add(panel);
-        pack();
+public class GameWindow extends JInternalFrame {
+    private GameVisualizer m_visualizer;
+
+    public GameWindow() {
+        super("Крутой робот", true, true, true, true);
+        RobotModel robotModel = new RobotModel();
+        m_visualizer = new GameVisualizer(robotModel);
+        add(m_visualizer);
+        setDefaultCloseOperation(JInternalFrame.DISPOSE_ON_CLOSE);
+        setSize(400, 400);
+        setLocation(50, 50);
     }
 }
